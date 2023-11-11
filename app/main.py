c3043d76f6d3defbc8efae8bc81ade28e3986c07
Luciano Filho <lvgalvaofilho@gmail.com>
2023-11-11 11:29:14 -0300

    database adicionada

--- app/main.py
+++ app/main.py
@@ -1,27 +1,8 @@
 from fastapi import FastAPI
 from .schema import ProdutosSchema
-from .data import Produtos
+from .routes import router
 from typing import List
 
 app = FastAPI()
-lista_de_produtos = Produtos()
 
-
-@app.get("/")
-def ola_mundo():
-    return {"Olá": "Mundo"}
-
-
-@app.get("/produtos", response_model=List[ProdutosSchema])
-def listar_produtos():
-    return lista_de_produtos.listar_produtos()
-
-
-@app.get("/produtos/{id}", response_model=ProdutosSchema)
-def buscar_produto(id: int):
-    return lista_de_produtos.buscar_produto(id)
-
-
-@app.post("/produtos", response_model=ProdutosSchema)
-def adicionar_produto(produto: ProdutosSchema):
-    return lista_de_produtos.adicionar_produtos(produto.model_dump())
+app.include_router(router)
--- tests/test_api.py
+++ tests/test_api.py
@@ -10,26 +10,6 @@ def test_ola_mundo_status_code():
     assert response.status_code == 200
 
 
-def test_ola_mundo_conteudo():
-    response = client.get("/")
-    assert response.json() == {"Olá": "Mundo"}
-
-
 def test_listar_produtos_status_code():
     response = client.get("/produtos")
     assert response.status_code == 200
-
-
-def test_tamanho_da_lista_de_produtos():
-    response = client.get("/produtos")
-    assert len(response.json()) == 3
-
-
-def test_pega_um_produto():
-    response = client.get("/produtos/1")
-    assert response.json() == {
-        "id": 1,
-        "nome": "Smartphone",
-        "descricao": "Um telefone que é inteligente",
-        "preco": 1500.0,
-    }
